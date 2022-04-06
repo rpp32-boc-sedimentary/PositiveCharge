23c5767958478726d9c47860be32e9dfab3f08c2
Dwight Doran <dwight.d.doran@gmail.com>
2022-04-05 23:07:31 -0500

    added all routes for poi details mvp

--- servers/index.js
+++ servers/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 var bodyParser = require('body-parser');
 const authRouter = require('./routes/authRoutes.js');
-
+const detailsRouter = require('./routes/detailsRoutes.js');
 
 const app = express();
 const port = 3000;
@@ -11,6 +11,7 @@ app.use(express.static(path.join(__dirname,'../positive_charge/public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use('/', authRouter);
+app.use('/details', detailsRouter);
 
 app.get('/', (req, res) => {
     res.send("Sarcastic hello");
