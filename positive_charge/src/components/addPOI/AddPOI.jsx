31f65f5dd9f3184d54cb6683a87c87121aaef6a2
Jeff Fox <jeffaveryfox@gmail.com>
2022-04-05 21:47:29 -0500

    Basic form inputs complete

--- positive_charge/src/components/addPOI/AddPOI.jsx
+++ positive_charge/src/components/addPOI/AddPOI.jsx
@@ -1,27 +1,27 @@
-import * as React from 'react'
+import React, { useState } from 'react'
 
 function AddPOI () {
-  // const [pointName, setPointName] = useState('')
-  // const [location, setLocation] = useState('')
-  // const [category, setCategory] = useState('')
-  // const [price, setPrice] = useState('')
-  // const [photo, setPhoto] = useState('')
+  const [pointName, setPointName] = useState('')
+  const [location, setLocation] = useState('')
+  const [category, setCategory] = useState('Food')
+  const [price, setPrice] = useState('')
+  const [photo, setPhoto] = useState('')
 
   return (
     <div className="add-poi-form-container">
       <div className="add-poi-form-wrapper">
         <h1>Add Point of Interest</h1>
         <form>
+
         <label htmlFor="point-name-input">Name of Point of Interest: </label>
         <input
           type="text"
           className="text-input"
           id="point-name-input"
           placeholder=""
-          // value={pointName}
-          // onChange={e => {setPointName(e.target.value)}}
+          value={pointName}
+          onChange={e => {setPointName(e.target.value)}}
           >
-
           </input><br></br>
 
           <label htmlFor="point-name-input">Location: </label>
@@ -30,22 +30,33 @@ function AddPOI () {
           className="text-input"
           id="location-input"
           placeholder=""
-          // value={location}
-          // onChange={e => {setLocation(e.target.value)}}
+          value={location}
+          onChange={e => {setLocation(e.target.value)}}
           ></input><br></br>
 
           <label htmlFor="category-select">Category: </label>
           <select id="category-select"
-          // value={category}
-          // placeholder="Please select a category"
-          // onChange={e => {setCategory(e.target.value)}}
-          >
+          value={category}
+          onChange={e => {setCategory(e.target.value)}}>
             <option value="food">Food</option>
             <option value="active">Active</option>
             <option value="culture">Culture</option>
             <option value="other">Other</option>
-          </select>
+          </select><br></br>
 
+          <h3>Price</h3>
+          <input type="radio" className="radio-button price-input" name="price" id="free" value="free"></input>
+          <label htmlFor="free">Free</label>
+
+          <input type="radio" className="radio-button price-input" name="price" id="$" value="$"></input>
+          <label htmlFor="$">$</label>
+
+          <input type="radio" className="radio-button price-input" name="price" id="$$" value="$$"></input>
+          <label htmlFor="$$">$$</label>
+
+          <input type="radio" className="radio-button price-input" name="price" id="$$$" value="$$$"></input>
+          <label htmlFor="$$$">$$$</label><br></br>
+          
         </form>
       </div>
     </div>
