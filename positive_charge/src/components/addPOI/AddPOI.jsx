2620df53396fe5d4fb20f1033927b0ec9753e32e
Jeff Fox <jeffaveryfox@gmail.com>
2022-04-05 22:56:58 -0500

    Skeleton of routing set up, no DB connection

--- positive_charge/src/components/addPOI/AddPOI.jsx
+++ positive_charge/src/components/addPOI/AddPOI.jsx
@@ -1,28 +1,52 @@
 import React, { useState } from 'react'
+import axios from 'axios'
 
 function AddPOI () {
   const [pointName, setPointName] = useState('')
   const [location, setLocation] = useState('')
-  const [category, setCategory] = useState('Food')
+  const [category, setCategory] = useState('food')
   const [price, setPrice] = useState('')
-  const [photo, setPhoto] = useState('')
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    let data = {
+      name: pointName,
+      location,
+      category,
+      price
+    }
+    if (validatePoiInfo(data)) {
+      axios.post('/addPOI', data)
+      .then(response => {
+        console.log('response in component', response)
+      })
+      .catch(err => {
+        console.error(err)
+      })
+    }
+  }
+
+  //placeholder validation function
+  function validatePoiInfo(data) {
+    return true
+  }
 
   return (
     <div className="add-poi-form-container">
       <div className="add-poi-form-wrapper">
-        <h1>Add Point of Interest</h1>
-        <form>
+        <h1>Add a Point of Interest</h1>
+        <form onSubmit={(e) => handleSubmit(e)}>
 
-        <label htmlFor="point-name-input">Name of Point of Interest: </label>
+        <label htmlFor="point-name-input">Name: </label>
         <input
           type="text"
           className="text-input"
           id="point-name-input"
           placeholder=""
           value={pointName}
-          onChange={e => {setPointName(e.target.value)}}
+          onChange={e => setPointName(e.target.value)}
           >
-          </input><br></br>
+          </input><br></br><br></br>
 
           <label htmlFor="point-name-input">Location: </label>
           <input
@@ -31,32 +55,33 @@ function AddPOI () {
           id="location-input"
           placeholder=""
           value={location}
-          onChange={e => {setLocation(e.target.value)}}
-          ></input><br></br>
+          onChange={e => setLocation(e.target.value)}
+          ></input><br></br><br></br>
 
           <label htmlFor="category-select">Category: </label>
           <select id="category-select"
           value={category}
-          onChange={e => {setCategory(e.target.value)}}>
+          onChange={e => setCategory(e.target.value)}>
             <option value="food">Food</option>
             <option value="active">Active</option>
             <option value="culture">Culture</option>
             <option value="other">Other</option>
-          </select><br></br>
+          </select><br></br><br></br>
 
           <h3>Price</h3>
-          <input type="radio" className="radio-button price-input" name="price" id="free" value="free"></input>
+          <input type="radio" className="radio-button price-input" name="price" id="free" value="free" onClick={e => setPrice(e.target.value)}></input>
           <label htmlFor="free">Free</label>
 
-          <input type="radio" className="radio-button price-input" name="price" id="$" value="$"></input>
+          <input type="radio" className="radio-button price-input" name="price" id="$" value="$" onClick={e => setPrice(e.target.value)}></input>
           <label htmlFor="$">$</label>
 
-          <input type="radio" className="radio-button price-input" name="price" id="$$" value="$$"></input>
+          <input type="radio" className="radio-button price-input" name="price" id="$$" value="$$" onClick={e => setPrice(e.target.value)}></input>
           <label htmlFor="$$">$$</label>
 
-          <input type="radio" className="radio-button price-input" name="price" id="$$$" value="$$$"></input>
-          <label htmlFor="$$$">$$$</label><br></br>
-          
+          <input type="radio" className="radio-button price-input" name="price" id="$$$" value="$$$" onClick={e => setPrice(e.target.value)}></input>
+          <label htmlFor="$$$">$$$</label><br></br><br></br>
+
+          <input type="submit" value="Add POI"></input>
         </form>
       </div>
     </div>
