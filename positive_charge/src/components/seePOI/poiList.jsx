6a46d5b7aacbc5afe939637453954822c504a2f7
DominicCarullo <dominiccarullo@msn.com>
2022-04-09 11:40:57 -0600

    list of poi implemented

---
+++ positive_charge/src/components/seePOI/poiList.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const PoiList = (props) => {
+  console.log('props', props);
+
+  if (props === undefined) {
+    return (
+      <div className='loading'>Loading POI</div>
+    )
+  } else if (props.props !== undefined) {
+    let eachPOI = props.props.businesses.map((item) => {
+      return (
+        <div key={item.id}>
+        <span className='POIName' >{item.name}         </span> <span className='likes'>{item.rating} yelp rating       </span> <span className='distance' >{props.walkTime(item.distance)} min walk</span>
+        </div>
+      )
+    })
+
+    return (
+      <div className='poiList'>
+        {eachPOI}
+      </div>
+    )
+  }
+  }
+
+export default PoiList;
