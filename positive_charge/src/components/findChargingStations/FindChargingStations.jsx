2484af40518dae8b3aa44f66f00c062b74238e5d
Dakota <lustdakota14@gmail.com>
2022-04-08 22:20:29 -0600

    Basic setup of my feature and a fix to the .yml

---
+++ positive_charge/src/components/findChargingStations/FindChargingStations.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class FindChargingStations extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  getUserLocation() {
+
+  }
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
