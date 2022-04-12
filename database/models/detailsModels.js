98487e180972182051d949d56f66ffb68e8c7236
Dwight Doran <dwight.d.doran@gmail.com>
2022-04-11 23:45:58 -0500

    connection to db success, added model files for every feature with test functions

--- database/models/detailsModels.js
+++ database/models/detailsModels.js
@@ -1,45 +1,73 @@
-const dummy = require('../dummyData/details.js');
+const { Pool } = require('pg');
 
-exports.detailsModels = {
+const pool = new Pool({
+  host: process.env.DB_Host,
+  user: process.env.DB_User,
+  port: process.env.DB_Port,
+  password: process.env.DB_Password,
+  database: process.env.DB_Name,
+  max: 50,
+  connectionTimeoutMillis: 1000,
+  idleTimeoutMillis: 1000
+});
 
-  grabview: async (poiId) => {
-    return dummy.detailData;
-  },
-
-  lovePoi: async (poiId) => {
-    dummy.detailData.poiLoves++;
-    return dummy.detailData;
-  },
-
-  flagPoi: async (poiId) => {
-    dummy.detailData.flagStatus = !dummy.detailData.flagStatus;
-    return dummy.detailData;
-  },
-
-  loveExp: async (expId) => {
-    dummy.detailData.experiences[expId].expLoves++;
-    return dummy.detailData;
-  },
-
-  flagExp: async (expId) => {
-    dummy.detailData.experiences[expId].flagStatus = !dummy.detailData.experiences[expId].flagStatus;
-    return dummy.detailData;
-  },
-
-  addExperience: async (exp) => {
-    dummy.detailData.experiences['4'] = {
-      expId: 4,
-      expLoves: 0,
-      flagStatus: false,
-      experience: exp,
-      photos: null
-    }
-    return dummy.detailData;
-  },
-
-  deleteExperience: async (expId) => {
-    delete dummy.detailData.experiences[expId];
-    return dummy.detailData;
-  }
-
+pool.lovePoi = async (poiId) => {
+  const query = `INSERT INTO test (name) VALUES ('this is a test')`;
+  const love = await pool.query(query)
+  console.log(love.rows)
+  return;
 }
+
+pool.grabview = async (poiId) => {
+  const query = `SELECT * FROM test`;
+  const love = await pool.query(query)
+  console.log(love.rows)
+  return
+}
+
+module.exports = { pool };
+
+// exports.detailsModels = {
+
+  // grabview: async (poiId) => {
+  //   return dummy.detailData;
+  // },
+
+  // lovePoi: async (poiId) => {
+
+  //   const query = `INSERT INTO test (name) VALUES ('this is a test')`;
+
+  // },
+
+//   flagPoi: async (poiId) => {
+//     dummy.detailData.flagStatus = !dummy.detailData.flagStatus;
+//     return dummy.detailData;
+//   },
+
+//   loveExp: async (expId) => {
+//     dummy.detailData.experiences[expId].expLoves++;
+//     return dummy.detailData;
+//   },
+
+//   flagExp: async (expId) => {
+//     dummy.detailData.experiences[expId].flagStatus = !dummy.detailData.experiences[expId].flagStatus;
+//     return dummy.detailData;
+//   },
+
+//   addExperience: async (exp) => {
+//     dummy.detailData.experiences['4'] = {
+//       expId: 4,
+//       expLoves: 0,
+//       flagStatus: false,
+//       experience: exp,
+//       photos: null
+//     }
+//     return dummy.detailData;
+//   },
+
+//   deleteExperience: async (expId) => {
+//     delete dummy.detailData.experiences[expId];
+//     return dummy.detailData;
+//   }
+
+// }
