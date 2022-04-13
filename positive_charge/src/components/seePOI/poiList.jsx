e84751a6938b243412701fd03fd7ecbe2a7904f1
DominicCarullo <dominiccarullo@msn.com>
2022-04-12 18:15:50 -0600

    basic outline of more details page with link to poiDetails modal, passing buisness data as props to Modal

--- positive_charge/src/components/seePOI/poiList.jsx
+++ positive_charge/src/components/seePOI/poiList.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import MoreDetails from './moreDetails.jsx'
+import { BrowserRouter, Routes, Route, Link, Navigate } from 'react-router-dom';
 
 const PoiList = (props) => {
 
@@ -8,10 +10,13 @@ const PoiList = (props) => {
     )
   } else if (props.props !== undefined) {
     let eachPOI = props.props.businesses.map((item, index) => {
+
       return (
+        <Link to='/moreDetails' state={{data:item}} >
         <div key={item.id}>
         <span className='listNumber'>{index + 1}</span><span className='POIName' >{item.name} </span> <span className='likes'>{item.rating} yelp rating </span> <span className='distance' >{props.walkTime(item.distance)} min walk</span>
         </div>
+        </Link>
       )
     })
 
