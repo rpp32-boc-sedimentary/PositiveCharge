55f86189da3ab45a31b0607c5c8d30b5ab11d59b
DominicCarullo <dominiccarullo@msn.com>
2022-04-15 20:53:42 -0600

    added testing file, adjusted test script to run jest

---
+++ tests/seePOI/seePOI.test.js
@@ -0,0 +1,11 @@
+// import {render, screen, cleanup } from '@testing-library/react';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import SeePOI from '../../positive_charge/src/components/seePOI/seePOI.jsx';
+
+// describe('SeePOI', () => {
+//   test('renders SeePOI component', () => {
+//     render(<SeePOI />);
+
+//   })
+// })
