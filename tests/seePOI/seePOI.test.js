0146889464828121578333410f9c3acabd9fca99
DominicCarullo <dominiccarullo@msn.com>
2022-04-16 00:12:53 -0600

    working testing changes

--- tests/seePOI/seePOI.test.js
+++ tests/seePOI/seePOI.test.js
@@ -1,11 +1,23 @@
-// import {render, screen, cleanup } from '@testing-library/react';
-// import React from 'react';
+/**
+ * @jest-environment jsdom
+ */
+
+// import '@testing-library/jest-dom';
+// import {render, fireEvent, screen, cleanup } from '@testing-library/react';
+// import * as React from 'react';
 // import ReactDOM from 'react-dom';
 // import SeePOI from '../../positive_charge/src/components/seePOI/seePOI.jsx';
 
+// jest.mock(SeePOI);
+
+// beforeEach(() => {
+//   SeePOI.mockClear()
+// })
 // describe('SeePOI', () => {
 //   test('renders SeePOI component', () => {
 //     render(<SeePOI />);
+//     expect(screen.getByText(/Experiences Near You/)).toBeInTheDocument();
 
 //   })
 // })
+
