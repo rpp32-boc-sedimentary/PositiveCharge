407ef522a4899f99adbc620fc221d198be2300b5
Dwight Doran <dwight.d.doran@gmail.com>
2022-04-22 15:09:11 -0500

    basic style complete for modal

--- positive_charge/src/components/poiDetails/PoiModal.jsx
+++ positive_charge/src/components/poiDetails/PoiModal.jsx
@@ -2,18 +2,19 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import {
   Dialog,
-  DialogTitle,
-  DialogContentText,
+  CardActions,
+  Grid,
   DialogActions,
-  Button,
+  CardContent,
   List,
-  ListItem,
+  Card,
   Divider,
   IconButton,
   Snackbar,
   BottomNavigation,
   BottomNavigationAction,
-  Typography
+  Typography,
+  Box
 } from '@mui/material';
 
 import FavoriteIcon from '@mui/icons-material/Favorite';
@@ -22,6 +23,7 @@ import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import ExpandLessIcon from '@mui/icons-material/ExpandLess';
 import AddIcon from '@mui/icons-material/Add';
 import CloseIcon from '@mui/icons-material/Close';
+import LightbulbIcon from '@mui/icons-material/Lightbulb';
 
 import AddExperience from './AddExperience.jsx'
 
@@ -67,38 +69,67 @@ export default function PoiModal({open, onClose, detail, name}) {
     let exp = showMore ? minExp = sortedDetails.length : minExp;
 
     return sortedDetails.slice(0, exp).map((exp, index) => {
-      return <ListItem key={index}>
-         <DialogContentText>{exp.experience}</DialogContentText>
+      return <Grid>
+          <Card
+            key={index}
+            variant="outlined"
+            sx={{
+              display: 'grid',
+              gridTemplateColumns: 'repeat(10, 1fr)',
+              gap: 1,
+              gridTemplateRows: 'auto',
+              gridTemplateAreas: `"ex ex ex ex ex ex ex ex ex ex ex"
+                                  "ex ex ex ex ex ex ex ex ex ex ex"
+                                  "ex ex ex ex ex ex ex ex ex ex ex"
+                                  "love . . . . . . . . . flag"`,
+            }}
+          >
+            <CardContent
+              sx={{
+                gridArea: 'ex' ,
+                bgcolor: 'primary.main',
 
-          <IconButton
-            onClick={() => {
-              love('experience', exp.experience)
-              setSnackbarLoveOpen(true)
               }}>
-            <FavoriteIcon/>{exp.exp_loves}
-          </IconButton>
-          <Snackbar
-            open={snackbarLoveOpen}
-            autoHideDuration={3000}
-            onClose={() => {setSnackbarLoveOpen(false)}}
-            message="Thanks for the love!"
-          />
-          <br/>
-          <br/>
-          <IconButton
-            onClick={() => {
-              flag('experience', exp.experience)
-              setSnackbarFlagOpen(true)
+              <Typography sx={{
+                fontSize: '1.25rem'
               }}>
-            <FlagIcon/>
-          </IconButton>
-          <Snackbar
-            open={snackbarFlagOpen}
-            autoHideDuration={3000}
-            onClose={() => {setSnackbarFlagOpen(false)}}
-            message="Thanks for informing us! We'll review your flag."
-          />
-       </ListItem>
+                {exp.experience}
+              </Typography>
+            </CardContent>
+            <CardActions
+              sx={{ gridArea: 'love' , fontSize:'small'}}>
+              <IconButton
+                onClick={() => {
+                  love('experience', exp.experience)
+                  setSnackbarLoveOpen(true)
+                  }}>
+                <FavoriteIcon/>{exp.exp_loves}
+              </IconButton>
+            <Snackbar
+              open={snackbarLoveOpen}
+              autoHideDuration={3000}
+              onClose={() => {setSnackbarLoveOpen(false)}}
+              message="Thanks for the love!"
+              />
+            </CardActions>
+            <CardActions
+            sx={{ gridArea: 'flag', fontSize:'small' }}>
+              <IconButton
+                onClick={() => {
+                  flag('experience', exp.experience)
+                  setSnackbarFlagOpen(true)
+                  }}>
+                <FlagIcon/>
+              </IconButton>
+              <Snackbar
+                open={snackbarFlagOpen}
+                autoHideDuration={3000}
+                onClose={() => {setSnackbarFlagOpen(false)}}
+                message="Thanks for informing us! We'll review your flag."
+              />
+            </CardActions>
+          </Card>
+       </Grid>
      })
 
   }
@@ -110,15 +141,9 @@ export default function PoiModal({open, onClose, detail, name}) {
       open={open}
       fullWidth={true}
     >
-      <DialogTitle
-        sx={{
-          fontSize: 50
-        }}
-      >{name?.props ? `Experiences at ${name.props.name}` : 'loading...'}</DialogTitle>
-
-      <List>
-        {detail[0]?.experience ? displayExperiences() : 'Be the first to add your experience!'}
-      </List>
+        <List>
+          {detail[0]?.experience ? displayExperiences() : 'Be the first to add your experience!'}
+        </List>
 
       {detail.length > 5 ?
         <IconButton variant='outlined' onClick={() => showMoreExp(!showMore)}>{showMore ? <ExpandLessIcon/> : <ExpandMoreIcon/>}</IconButton>
@@ -126,62 +151,64 @@ export default function PoiModal({open, onClose, detail, name}) {
 
       <Divider />
 
-      <DialogActions>
-        <Snackbar
-            open={snackbarLoveOpen}
-            autoHideDuration={4000}
-            onClose={() => {setSnackbarLoveOpen(false)}}
-            message="Thanks for the love!"
+      <Box component="footer">
+        <DialogActions>
+          <Snackbar
+              open={snackbarLoveOpen}
+              autoHideDuration={3000}
+              onClose={() => {setSnackbarLoveOpen(false)}}
+              message="Thanks for the love!"
+            />
+        </DialogActions>
+
+        <BottomNavigation>
+          <BottomNavigationAction
+            label="Love"
+            value="love"
+            icon={<FavoriteIcon />}
+            onClick={() => {
+              love('poi')
+              setSnackbarLoveOpen(true)
+              }}
           />
-      </DialogActions>
 
-      <BottomNavigation>
-        <BottomNavigationAction
-          label="Love"
-          value="love"
-          icon={<FavoriteIcon />}
-          onClick={() => {
-            love('poi')
-            setSnackbarLoveOpen(true)
-            }}
-        />
+          <BottomNavigationAction
+            label="Share Experience"
+            value="share"
+            icon={<LightbulbIcon />}
+            onClick={() => setIsOpen(true)}
+          />
 
-        <BottomNavigationAction
-          label="Share Experience"
-          value="share"
-          icon={<AddIcon />}
-          onClick={() => setIsOpen(true)}
-        />
+          <BottomNavigationAction
+            label="Flag"
+            value="flag"
+            icon={<FlagIcon />}
+            onClick={() => {
+              flag('poi')
+              setSnackbarFlagOpen(true)
+              }}
+          />
 
-        <BottomNavigationAction
-          label="Flag"
-          value="flag"
-          icon={<FlagIcon />}
-          onClick={() => {
-            flag('poi')
-            setSnackbarFlagOpen(true)
-            }}
-        />
+          <BottomNavigationAction
+            label="Close"
+            value="close"
+            icon={<CloseIcon />}
+            onClick={onClose}
+          />
 
-        <BottomNavigationAction
-          label="Close"
-          value="close"
-          icon={<CloseIcon />}
-          onClick={onClose}
-        />
+        </BottomNavigation>
 
-      </BottomNavigation>
+        <DialogActions>
+          <AddExperience open={isOpen} onClose={() => setIsOpen(false)} name={name}></AddExperience>
+        </DialogActions>
 
-      <DialogActions>
-        <AddExperience open={isOpen} onClose={() => setIsOpen(false)} name={name}></AddExperience>
-      </DialogActions>
-
-      <Snackbar
-          open={snackbarFlagOpen}
-          autoHideDuration={4000}
-          onClose={() => {setSnackbarFlagOpen(false)}}
-          message="Thanks for informing us! We'll review your flag."
-        />
+        <Snackbar
+            open={snackbarFlagOpen}
+            autoHideDuration={3000}
+            onClose={() => {setSnackbarFlagOpen(false)}}
+            message="Thanks for informing us! We'll review your flag."
+          />
+      </Box>
 
     </Dialog>
   );
