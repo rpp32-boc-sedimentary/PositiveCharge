6a42f25dc7511cdb06c2179e8fc9ce9cecb4b889
DominicCarullo <dominiccarullo@msn.com>
2022-04-22 18:51:28 -0600

    styling

--- positive_charge/src/components/seePOI/seePOI.jsx
+++ positive_charge/src/components/seePOI/seePOI.jsx
@@ -177,17 +177,17 @@ class SeePOI extends React.Component {
 
   render() {
     return(
-
+    <ThemeProvider theme={theme}>
       <div className='seePOI'>
         <div className='returnCharger'><Link to='/'>Find a different charger</Link></div>
         <div className='login'><Link to='/login'>Log In</Link></div><div className='signup'><Link to='/signup'>Sign up</Link></div>
-        <h3 className='seePOIListHeader'>Experiences Near You</h3> <br></br>
+        <div className='seePOIListHeader'>Experiences Near You</div><br></br>
         <div className='map'> {this.state.mapData !== undefined ? <Map props={this.state.mapData} userLocation={{userLat: this.state.lat, userLong: this.state.long}}></Map> : <div className='loading'> Loading...</div>}</div>
         <div className='poiList'>{this.state.data !== undefined ? <PoiList props={this.state.data} walkTime={this.walkTime}></PoiList> : <div className='loading'> Loading...</div>} </div>
         <div className='filters'>{this.state.flag !== undefined ? <LittleFilter changeDisplay={this.changeDisplay} userLocation={{lat: this.state.lat, long: this.state.long}} allData={{all: this.state.all, database:this.state.database, food: this.state.food, cafes:this.state.cafes, lAndH:this.state.lAndH, museums:this.state.museums, parks:this.state.parks}} exampleInputForCDfunc={this.state.data}/> : <div className='loading'> Loading...</div>} </div><br></br>
         <div className='addPOI'><Link to='/addPOI'>Add a Point of Interest</Link></div>
       </div>
-
+    </ThemeProvider>
     )
 
   }
